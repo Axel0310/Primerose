d1226faa68c2e4ff800ddfbdc37d5566c24dae17
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-13 17:41:58 +0200

    Minor change on product model

--- models/Product.js
+++ models/Product.js
@@ -28,7 +28,6 @@ const productSchema = new Schema({
         enum: ["Coats & Jackets", "Dresses", "Sweatshirts","Jeans","Jumpsuits","Shorts","Skirts","Suits","Swimwear","Tops", "Tracksuits","Trousers","Workwear","Lingerie"]
     },
     sizesAvailable: [{
-        type: String,
         size : {
             type : String,
             enum: ["XXS", "XS", "S", "M", "L", "XL", "XXL"],
