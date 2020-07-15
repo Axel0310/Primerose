559a98e75c001a038f70dfd0221066689e24134e
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-15 10:01:45 +0200

    Put cart route in user route

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 app.use('/products', require('./routes/products'));
 app.use('/shops', require('./routes/shops'));
-app.use('/cart', require('./routes/cart'));
+app.use('/user', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post("/cart", async (req, res, next) =>{
+    try {
+        const product = await productModel.create(req.body);
+        // let subtotal = req.body.select-quantity*req.body.select-size;
+        res.render("cart", {product})
+    } catch (error) {
+        next(error)
+    }
+});
+
 module.exports = router;
