97c01f7c6b9b7b14db783935fe16b0c0f503c856
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-15 17:51:00 +0200

    Add search bar route to index.js

--- public/javascript/searchbar.js
+++ public/javascript/searchbar.js
@@ -1,15 +1,18 @@
 let searchBar = document.getElementById("search-bar");
 let searchBtn = document.getElementById("search-button");
 
-searchBtn.onclick = console.log("the search btn was clicked yay!");
-
-function search_product() {
+async function searchProduct() {
     let input = document.getElementById('search-bar').value ;
+    console.log("the input is >>>", input);
     input=input.toLowerCase(); 
 const products = await productModel.insertMany();
+    console.log("here is the list of products >>>", products);
+
 for (let i=0; i<products.length; i++) {
     if(products[i].name.includes(input)) {
         console.log("here are the search results >>>>", products[i]);
     }
 }
 }
+
+searchBtn.onclick = searchProduct; 
--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,36 @@ router.get('/', async (req, res, next) => {
   const topShops = await shopModel.find().sort({view:-1}).limit(5);
   const newShops = await shopModel.find().sort({date:1}).limit(5);
   const tagsList = await categoryModel.find();
-  res.render('home', { title: 'Home', topShops: topShops, newShops: newShops, tagsList: tagsList, scripts:["searchbar"]});
+  res.render('home', { title: 'Home', topShops: topShops, newShops: newShops, tagsList: tagsList});
   }
   catch (error) {
     next(error);
   }
 });
+
+router.get("/search-results", async (req,res,next) => {
+  try {
+    let searchResults = []
+    let input = req.body;
+    console.log("the input is >>>", input);
+
+    input=input.toLowerCase(); 
+
+  const products = await productModel.insertMany();
+    console.log("here is the list of products >>>", products);
+
+for (let i=0; i<products.length; i++) {
+    if(products[i].name.includes(input)) {
+        console.log("here are the search results >>>>", products[i]);
+        searchResults.push(products[i]);
+        return searchResults;
+    }
+}
+res.render("search_results", {searchResults});
+  }
+  catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
