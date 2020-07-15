40067d34ad60ea3854eac290e9e71bdd083ef1c7
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-15 16:02:38 +0200

    Create searchbar partials

--- middlewares/devMode.js
+++ middlewares/devMode.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
       _id: "5f0edd00d95474138812d985",
       profil: "user",
       email: "admin@fake.com",
-      shop: "5f0dee0189598d494f5bd710"
+      shop: "5f0dbded20aa9136ac2bbafe"
     };
     next();
   };
