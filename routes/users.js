54d6d181ded1ce06a6307b1da668d4e5cee63ee5
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-16 12:19:58 +0200

    Update cart total and subtotal when when quantity changed

--- routes/users.js
+++ routes/users.js
@@ -59,4 +59,35 @@ router.get("/cart/delete/:id", async (req, res, next) => {
   }
 });
 
+router.get("/cart/update/:id", async (req,res,next) => {
+  try {
+  let cart = req.session.userCart; 
+
+   const isProductId = (productInfo) => {
+     return productInfo.id === req.params.id;   }
+   cart.findIndex(isProductId);
+   const productIndex = cart.findIndex(isProductId);
+
+   cart[productIndex].quantity = req.query.quantity;
+  console.log("this is the new cart>>>", cart);
+
+  let total = 0;
+
+  cart.forEach( (productInfo) => {
+    let productPrice = Number(productInfo.price);
+    let productQuantity = Number(productInfo.quantity);
+  
+  const subtotal = productPrice*productQuantity;
+  productInfo.subtotal = subtotal;
+  total +=subtotal;
+    console.log("cart>>>", cart);
+  })
+
+res.render("cart", {cart, total});
+  }
+  catch (error) {
+    next(error);
+  }
+})
+
 module.exports = router;
