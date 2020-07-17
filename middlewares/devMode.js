469067afc1242893ba67e688c44aeec6af57314c
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-17 09:27:07 +0200

    add new currentuser id for devmode

--- middlewares/devMode.js
+++ middlewares/devMode.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     req.session.currentUser = {
-      _id: "5f0f7dea5a48c266b19c8c0d",
+      _id: "5f102bf6b4a8a565af3a8ac8",
       profil: "user",
       email: "admin@fake.com",
       shop: "5f0f06eae0697e3bb1d080eb",
