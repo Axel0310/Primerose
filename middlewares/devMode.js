49266148dd9de7aaddc7cd15e33f59c979199ebb
Axel Chauvin <axelchauvin@gmail.com>
2020-07-17 08:10:19 +0200

    Completed css

--- middlewares/devMode.js
+++ middlewares/devMode.js
@@ -1,9 +1,13 @@
 module.exports = (req, res, next) => {
     req.session.currentUser = {
-      _id: "5f0edd00d95474138812d985",
+      _id: "5f0f7dea5a48c266b19c8c0d",
       profil: "user",
       email: "admin@fake.com",
-      shop: "5f0f06eae0697e3bb1d080eb"
+      shop: "5f0f06eae0697e3bb1d080eb",
+      favoriteShops: [],
+      favoriteProducts: [],
+      pastOrders: []
+
     };
     next();
   };
